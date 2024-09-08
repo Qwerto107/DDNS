2336d1e3308437c064f6141c01bb83219cb3c253
Frencis107 <25398342+Qwerto107@users.noreply.github.com>
2024-09-08 20:31:54 +0800

    Docker 运行时显示日志

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 if [ $# -eq 0 ]; then
   printenv > /etc/environment
   echo "*/3 * * * *   /ddns -c /config.json" > /etc/crontabs/root
-  exec crond -f
+  exec crond -f -L 3
 else
   first=`echo $1 | cut -c1`
   if [ "$first" = "-" ]; then
